b342b6193101bda619e40f5f1f4fddcc0746d0de
Brian Attwell <attwellBrian@gmail.com>
2015-10-27 12:13:55 -0700

    Typo in comment

--- app/src/main/java/com/brianattwell/noise/NoiseConstants.java
+++ app/src/main/java/com/brianattwell/noise/NoiseConstants.java
@@ -5,7 +5,7 @@ public interface NoiseConstants {
     double AMPLITUDE_RAMP_DURATION_MS = 1000 * 60 * 15.0;
     int NOISE_START_DELAY = 1000 * 60 * 60 * 5;
     /**
-     * Sample rate that white noise will get generated at. In order to be true white noise this needs to be 20 kHZ.
+     * Sample rate that white noise will get generated at. In order to be true white noise this needs to be 40 kHZ.
      * But I don't like true white noise.
      */
     int SAMPLE_RATE_HZ = 5000;
